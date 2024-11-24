cecf30d647a446be4d5a5169dcdd8af3f0706fc3
Bence Puschmann <bence.puschmann@lombiq.com>
2024-11-23 16:42:40 -0800

    Passing analyzer result to response

--- server/routes/analyze.js
+++ server/routes/analyze.js
@@ -9,10 +9,14 @@ router.get("/", (req, res) => {
     if (err) {
       // log and return if we encounter an error
       console.error("could not execute command: ", err);
-      return;
+
+      return res.status(400).json({ message: "could not execute command!" });
     }
     // log the output received from the command
     console.log("Output: \n", output);
+    console.log(output.trim().length === 0);
+    //return res.status(200).json({ message: output || "no syntax violation found!"});
+    return res.status(200).json({ message: output.trim().length === 0 ? "no syntax violation found!": output});
   });
 });
 
